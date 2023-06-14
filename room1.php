b3ab27b3a71f2b737940f1ec5f0d96faefc07473
Thawat <97440224+Thawat09@users.noreply.github.com>
2023-06-14 23:48:31 +0700

    sidebar

--- room1.php
+++ room1.php
@@ -1,7 +1,7 @@
 <?php
 include 'models/session_model.php';
-include 'views/sidebar.php';
-include 'views/navbar.php';
+// include 'views/sidebar.php';
+include 'views/navbar1.php';
 include 'models/logout_model.php';
 include 'models/room_1_model.php';
 include 'models/count.php';
--- views/navbar1.php
+++ views/navbar1.php
@@ -5,4 +5,7 @@
     <div class="navbar-center">
         <span class="navbar-text" style="color: white;">Equipment Inspection System</span>
     </div>
+    <a class="active" href="index.php">
+        <img src="./public/home1.png" alt="Logo" class="navbar-logo">
+    </a>
 </div>
