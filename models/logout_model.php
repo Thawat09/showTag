fb0d11cba59d3e24d63cf1ea88c796d9bdcf4da5
Thawat <97440224+Thawat09@users.noreply.github.com>
2023-06-14 22:59:06 +0700

    แก้ไข session_start

--- models/logout_model.php
+++ models/logout_model.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 if (isset($_POST['logout'])) {
     $_SESSION['loggedin'] = 'NO';
     session_destroy();
