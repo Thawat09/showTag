18b8044a4123f7c10e31a2bf554fcfe805c35a5c
Thawat <97440224+Thawat09@users.noreply.github.com>
2023-06-16 22:10:43 +0700

    ลดจำนวน navbar

--- room.php
+++ room.php
@@ -1,5 +1,5 @@
 <?php
 include 'models/session_model.php';
-include 'views/navbar1.php';
+include 'views/navbar.php';
 include 'models/logout_model.php';
 include 'views/room.php';
--- views/navbar.php
+++ views/navbar.php
@@ -5,4 +5,14 @@
     <div class="navbar-center">
         <span class="navbar-text" style="color: white;">Equipment Inspection System</span>
     </div>
+    <?php
+        $currentPage = basename($_SERVER['PHP_SELF']);
+        if ($currentPage === 'room.php') {
+            echo '
+                <a class="active" href="index.php">
+                    <img src="./public/home4.png" onclick="indexFunction()" alt="Logo" class="navbar-logo1">
+                </a>
+            ';
+        }
+    ?>
 </div>
